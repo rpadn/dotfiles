1bb6ff3ab8295b9ecf571b0273cbed8c4726397a
Riccardo Padoan <riccardo.padoan@exb.de>
2023-01-26 08:47:24 +0000

    misc: switch to dual 4k monitors

---
+++ .screenlayout/home-dual-4k-60hz.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 2560x1440 --pos 6000x2160 --rotate normal --output HDMI-1 --primary --mode 3840x2160 --pos 2160x0 --rotate normal --output DP-1 --mode 3840x2160 --pos 0x0 --rotate left --output DP-2 --off --output DP-3 --off --output DP-4 --off
