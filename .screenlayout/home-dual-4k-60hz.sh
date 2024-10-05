beb24fac6d45668e022aedd5db1a93a87609498c
Riccardo Padoan <riccardo.padoan@exb.de>
2024-10-05 14:32:01 +0100

    xrandr: latest changes for 2 monitors

--- .screenlayout/home-dual-4k-60hz.sh
+++ .screenlayout/home-dual-4k-60hz.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output HDMI-1 --mode 3840x2160 --pos 0x0 --rotate left --output DP-1 --primary --mode 3840x2160 --pos 2160x892 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off
+xrandr --output eDP-1 --off --output HDMI-1 --mode 3840x2160 --pos 3840x0 --rotate right --output DP-1 --primary --mode 3840x2160 --pos 0x755 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off
